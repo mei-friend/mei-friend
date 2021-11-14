cb6b8a902b0a80f1a391a7dc63367a09c5b0e758
Werner Goebl <goebl@mdw.ac.at>
2021-11-14 23:09:33 +0100

    worker thread sketched

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 var cm;
-var tk;
+var vrvWorker;
+var tkVersion;
+var mei;
 import {
   setOrientation,
   addResizerHandlers
 } from './resizer.js'
-import * as verovio from "./verovio-toolkit-wasm.js";
-
-// with node.js
-// var verovio = require( './verovio-toolkit-wasm.js' );
 
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -23,7 +21,8 @@ document.addEventListener('DOMContentLoaded', function() {
       //   header: "none",
       //   footer: "none"
       // });
-      myTextarea.value = meiXML;
+      mei = meiXML;
+      myTextarea.value = mei;
     });
 
   cm = CodeMirror.fromTextArea(myTextarea, {
@@ -44,16 +43,60 @@ document.addEventListener('DOMContentLoaded', function() {
   // editor.foldCode(CodeMirror.Pos(3, 0));
   addResizerHandlers(cm);
   window.onresize = () => setOrientation(cm);
+
+
+  console.log('DOMContentLoaded. Trying now to load Verovio...');
+  document.querySelector(".notation").innerHTML = "<b>Loading Verovio.</b>";
+
+  vrvWorker = new Worker('./worker.js');
+
+  vrvWorker.onmessage = handleFirstWorkerEvent;
+
+  // ask worker to load Verovio
+  vrvWorker.postMessage({
+    'cmd': 'loadVerovio'
+  });
+
+  // Verovio.module.onRuntimeInitialized = async _ => {
+  //   tk = new verovio.toolkit();
+  //   tkVersion = tk.getVersion();
+  //   console.log("Verovio " + tkVersion + ' loaded.');
+
+
 });
 
-document.addEventListener("DOMContentLoaded", (event) => {
-  document.querySelector(".notation").innerHTML = "<b>Loading Verovio.</b>";
-  Verovio.module.onRuntimeInitialized = async _ => {
-    tk = new verovio.toolkit();
-    tkVersion = tk.getVersion();
-    console.log("Verovio " + tkVersion + ' loaded.');
-    document.querySelector(".notation").innerHTML =
-      `Gratefully using&nbsp;
-         <a href="https://www.verovio.org/">Verovio ${tkVersion}</a>.`;
+
+function handleFirstWorkerEvent(e) {
+  console.log('main(). Worker said: ', e.data);
+  switch (e.data.cmd) {
+    case 'vrvLoaded':
+      console.info('EventListener vrvLoaded: ', this);
+      tkVersion = e.data.msg;
+      document.querySelector(".notation").innerHTML =
+        `Gratefully using&nbsp;
+       <a href="https://www.verovio.org/">Verovio ${tkVersion}</a>.`;
+      vrvWorker.postMessage({
+        'cmd': 'loadData',
+        'msg': `${mei}`
+      });
+      break;
+    case 'pageCount':
+      console.info(e.data.msg + ' pages.');
+      vrvWorker.postMessage({
+        'cmd': 'getPage',
+        'msg': '1'
+      });
+      break;
+    case 'svg':
+      document.querySelector('.notation').innerHTML = e.data.msg;
+      // this.activateSubscriptions(this.vrvWorker);
+      // worker.postMessage({
+      //   'cmd': 'setOptions',
+      //   'msg': vrvOptions
+      // });
   }
-});
+}
+
+export function setLayout(pos) {
+  setOrientation(cm, pos);
+}
---
+++ worker.js
@@ -0,0 +1,136 @@
+// var Verovio = require('verovio');
+importScripts('verovio-toolkit.js');
+// speed worker
+
+// see also:
+// https://github.com/DDMAL/Neon/pull/426/commits/f52981ad0927644927e5249f9fbe4ab6e9413f86
+// https: //github.com/DDMAL/Neon/blob/master/src/workers/VerovioWorker.js
+
+var tk;
+loadVerovio = function() {
+  /* create the worker toolkit instance */
+  console.log('Loading toolkit...');
+  tk = new verovio.toolkit();
+  result = {
+    'cmd': 'vrvLoaded',
+    'msg': tk.getVersion()
+  };
+  console.log('...done.')
+  postMessage(result);
+}
+loadVerovio.bind(this);
+
+onmessage = function(e) {
+  var data = e.data;
+  console.info("Worker received: ", data.cmd);
+  let result = '';
+  switch (data.cmd) {
+    case 'loadVerovio':
+      Module.onRuntimeInitialized = loadVerovio();
+      break;
+    case 'getAvailableOptions':
+      result = {
+        'cmd': 'availableOptions',
+        'msg': tk.getAvailableOptions()
+      };
+      break;
+    case 'getVersion':
+      result = {
+        'cmd': 'version',
+        'msg': tk.getVersion()
+      };
+      break;
+    case 'setOptions':
+      tk.setOptions(data.msg);
+      result = {
+        'cmd': 'optionsSet',
+        'msg': true
+      };
+      break;
+    case 'loadData':
+      try {
+        tk.loadData(data.msg);
+        result = {
+          'cmd': 'pageCount',
+          'msg': tk.getPageCount()
+        };
+      } catch (e) {
+        console.log(e);
+        return;
+      }
+      break;
+    case 'redoLayout':
+      try {
+        tk.redoLayout();
+        result = {
+          'cmd': 'pageCount',
+          'msg': tk.getPageCount()
+        };
+      } catch (e) {
+        console.log(e);
+        return;
+      }
+      break;
+    case 'getPage':
+      try {
+        result = {
+          'cmd': 'svg',
+          'msg': tk.renderToSVG(data.msg)
+        };
+      } catch (e) {
+        console.log(e);
+        return;
+      }
+      break;
+    case 'getMEI':
+      try {
+        result = {
+          'cmd': 'mei',
+          'msg': tk.getMEI(data.msg)
+        };
+      } catch (e) {
+        console.log(e);
+        return;
+      }
+      break;
+    case 'getTimeForElement':
+      try {
+        result = {
+          'cmd': 'timeForElement',
+          'msg': tk.getTimeForElement(data.msg)
+        };
+      } catch (e) {
+        console.log(e);
+        return;
+      }
+      break;
+    case 'getElementAttr':
+      try {
+        result = {
+          'cmd': 'elementAttr',
+          'msg': tk.getElementAttr(data.msg)
+        };
+      } catch (e) {
+        console.log(e);
+        return;
+      }
+      break;
+    case 'stop':
+      result = {
+        'cmd': 'stopped',
+        'msg': 'Worker stopped: ' + data.msg + '.'
+      };
+      close(); // Terminates the worker.
+      break;
+    default:
+      result = {
+        'cmd': data.msg,
+        'msg': 'Unknown command: ' + data.msg
+      };
+  };
+  if (result) postMessage(result);
+}
+
+// self.addEventListener('message', function(e) {
+//   self.postMessage(e.data);
+// }, false);
