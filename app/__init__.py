65cf0ef35facbf3cac911bf0acb37bdd33ba6103
Stefan Szepe <szepe.stefan@gmail.com>
2021-12-22 15:56:09 +0100

    added static_url_path and static folder to app

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,9 @@ from flask import Flask
 from os import getenv
 from authlib.integrations.flask_client import OAuth
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/mei-friend/static", static_folder='static')
 app.secret_key = getenv("FLASK_SECRET")
 
 from app import routes
+
 
