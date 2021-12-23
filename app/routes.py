34a97e1cfb503654febe2e300e8923af9fefa2ba
David M. Weigl <weigl@mdw.ac.at>
2021-12-23 11:20:31 +0100

    force either BOTH or NEITHER of STATIC_FOLDER and STATIC_URL_PATH to be configured

--- app/routes.py
+++ app/routes.py
@@ -24,11 +24,10 @@ github = oauth.register(
 def index():
     return render_template('index.html',
             isLoggedIn = False
-            )
+    )
 
 @app.route("/login")
 def login():
-    print(getenv("CLIENT_ID"))
     env_url = getenv("REDIRECT_URL")
     redirect_url = env_url if env_url else url_for("authorize", _external=True)
     # redirect_url = url_for("authorize", _external=True)
