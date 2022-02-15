915db82f93a58569bd7afe5c7aaa902296f71f0c
Werner Goebl <goebl@mdw.ac.at>
2022-02-15 16:11:55 +0100

    Update date

--- app/static/lib/main.js
+++ app/static/lib/main.js
@@ -164,7 +164,7 @@ import schema_meiAll from '../schemaInfo/mei-CMN-4.0.1.schemaInfo.js';
 
 // mei-friend version and date
 const version = 'develop-0.3.2';
-const versionDate = '14 Feb 2022';
+const versionDate = '15 Feb 2022';
 // const defaultMeiFileName = `${root}Beethoven_WoOAnh5_Nr1_1-Breitkopf.mei`;
 const defaultMeiFileName = `${root}Beethoven_WoO70-Breitkopf.mei`;
 const defaultVerovioOptions = {
