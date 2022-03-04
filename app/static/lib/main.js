712264fd700ef9ed25e264078d24238bc1f2d949
David M. Weigl <weigl@mdw.ac.at>
2022-03-04 17:52:19 +0100

    update version

--- app/static/lib/main.js
+++ app/static/lib/main.js
@@ -170,7 +170,7 @@ import {
 import default_schema from '../schemaInfo/mei-CMN-4.0.1.schemaInfo.js';
 
 // mei-friend version and date
-const version = 'develop-0.3.5';
+const version = '0.3.5';
 const versionDate = '4 March 2022';
 // const defaultMeiFileName = `${root}Beethoven_WoOAnh5_Nr1_1-Breitkopf.mei`;
 const defaultMeiFileName = `${root}Beethoven_WoO70-Breitkopf.mei`;
