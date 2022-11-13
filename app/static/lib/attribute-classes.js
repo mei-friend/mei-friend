72b0482c2e114b83f732cdd422a7fcbe46cb955f
Werner Goebl <goebl@mdw.ac.at>
2022-11-13 12:20:19 +0100

    Beautify attribute-classes

--- app/static/lib/attribute-classes.js
+++ app/static/lib/attribute-classes.js
@@ -1,8 +1,11 @@
-// lists of elements contained by attribute classes
+/*
+ * Lists of elements contained by attribute classes, MEI 4.0.1
+ */
 
+// elements with @curveDir
 export const attCurvature = ['bend', 'curve', 'lv', 'phrase', 'slur', 'tie'];
 
-// see att.placement in MEI 4.0.1 definition: 'measure' taken out
+// see att.placement @place in MEI 4.0.1 definition: 'measure' taken out
 export const attPlacement = [
   'accid', 'artic', 'attacca', 'breath', 'caesura', 'cpMark', 'dir', 'dynam',
   'dynam', 'f', 'fermata', 'fing', 'fingGrp', 'hairpin', 'harm', 'harpPedal',
@@ -10,22 +13,26 @@ export const attPlacement = [
   'refrain', 'reh', 'sp', 'stageDir', 'syl', 'tempo', 'trill', 'turn', 'verse'
 ];
 
-// pedal not yet in vertical group, but supported by Verovio; attacca, tempo not supported
+// elements with @vgrp (pedal not yet in vertical group, but supported by Verovio)
+// attacca, tempo not supported
 export const attVerticalGroup = [
   'attacca', 'dir', 'dynam', 'hairpin', 'tempo', 'pedal'
 ];
 
+// elements with @place
 export const dataPlacement = ['above', 'within', 'between', 'below'];
 
+// elements with @stem.dir
 export const attStems = ['note', 'chord', 'ambNote'];
 
-// not sure whether all listed...
+// control elements (not sure whether all listed...)
 export const modelControlEvents = ['anchoredText', 'arpeg', 'bracket',
   'bracketspan', 'breath', 'dir', 'dynam', 'fermata', 'fing', 'gliss',
   'hairpin', 'harm', 'mordent', 'mnum', 'octave', 'pedal', 'phrase', 'reh',
   'slur', 'tempo', 'tie', 'trill', 'turn'
 ];
 
+// array of pitch names
 export const pnames = ['c', 'd', 'e', 'f', 'g', 'a', 'b'];
 
 // according to Verovio 3.9's implementation of timeSpanningInterface()
