2847bfc9538b63d53c6346a6036ac443a5dfa77d
Werner Goebl <goebl@mdw.ac.at>
2023-04-19 22:11:38 +0200

    Add more mei-friend settings to lang packs en/de

--- app/static/lang/lang.de.js
+++ app/static/lang/lang.de.js
@@ -1,6 +1,9 @@
 /**
  * Language file for German Deutsch
  */
+
+import * as att from '../lib/attribute-classes.js';
+
 export const lang = {
   // Main menu bar
   GithubLoginLink: { text: 'Login' },
@@ -48,7 +51,10 @@ export const lang = {
   FacsimileRight: { text: 'Faksimile rechts' },
 
   // mei-friend SETTINGS MENU
-//   titleGeneral: { text: 'Allgemein' },
+  titleGeneral: {
+    text: 'Allgemein',
+    description: 'Allgemeine mei-friend Einstellungen',
+  },
   selectToolkitVersion: {
     text: 'Verovio Version',
     description:
@@ -80,5 +86,278 @@ export const lang = {
       'von Anwendungsname, Version, sowie Datum der ersten ' +
       'und letzten Bearbeitung,',
   },
-  selectLanguage: { text: 'Sprache', description: 'Wählen Sie die Sprache des mei-friend Interface.' },
+  selectLanguage: {
+    text: 'Sprache',
+    description: 'Wählen Sie die Sprache des mei-friend Interface.',
+  },
+  // Drag select
+  dragSelection: {
+    text: 'Elementauswahl mit der Maus',
+    description: 'Element durch Klicken und Ziehen mit der Maus auswählen',
+  },
+  dragSelectNotes: {
+    text: 'Noten auswählen',
+    description: 'Noten auswählen',
+  },
+  dragSelectRests: {
+    text: 'Pausen auswählen',
+    description: 'Pausen und Wiederholungen auswählen (rest, mRest, beatRpt, halfmRpt, mRpt)',
+  },
+  dragSelectControlElements: {
+    text: 'Platzierungs-Elemente auswählen',
+    description:
+      'Platzierungs-Elemente auswählen (d.h. jene mit einem @placement Attribut: ' + att.attPlacement.join(', ') + ')',
+  },
+  dragSelectSlurs: {
+    text: 'Bögen auswählen ',
+    description: 'Bögen auswählen (d.h. Elemente mit einem @curvature Attribut: ' + att.attCurvature.join(', ') + ')',
+  },
+  dragSelectMeasures: {
+    text: 'Takte auswählen ',
+    description: 'Takte auswählen (measures)',
+  },
+  // Control menu
+  controlMenuSettings: {
+    text: 'Notationskontrollleiste',
+    description: 'Elemente in der Notationskontrollleiste definieren',
+  },
+  controlMenuFlipToPageControls: {
+    text: 'Automatisch Blättern',
+    description: 'Steuerung anzeigen zum automatisches Blättern zur aktuellen Editor-Position ',
+  },
+  controlMenuUpdateNotation: {
+    text: 'Automatische Notationsaktualisierung',
+    description: 'Steuerung anzeigen zur automatischen Notationsaktualisierung',
+  },
+  controlMenuFontSelector: {
+    text: 'Auswahl der Notenschrift',
+    description: 'Auswahl der Notenschrift (SMuFL font) in der Notationskontrollleiste anzeigen',
+  },
+  controlMenuNavigateArrows: {
+    text: 'Navigationspfeile anzeigen',
+    description: 'Anzeigen der Navigationspfeile in der Notationskontrollleiste',
+  },
+  controlMenuSpeedmodeCheckbox: {
+    text: 'Speed Mode Auswahl anzeigen',
+    description: 'Speed Mode Auswahl anzeigen in der Notationskontrollleiste',
+  },
+  controlMenuSettings: {
+    text: 'Notation control bar',
+    description: 'Define items to be shown in control menu above the notation',
+  },
+  controlMenuFlipToPageControls: {
+    text: 'Show flip to page controls',
+    description: 'Show flip to page controls in notation control menu',
+  },
+  controlMenuUpdateNotation: {
+    text: 'Show notation update controls',
+    description: 'Show notation update behavior controls in notation control menu',
+  },
+  controlMenuFontSelector: {
+    text: 'Show notation font selector',
+    description: 'Show notation font (SMuFL) selector in notation control menu',
+  },
+  controlMenuNavigateArrows: {
+    text: 'Show navigation arrows',
+    description: 'Show notation navigation arrows in notation control menu',
+  },
+  controlMenuSpeedmodeCheckbox: {
+    text: 'Show speed mode checkbox',
+    description: 'Show speed mode checkbox in notation control menu',
+  },
+  // MIDI Playback
+  titleMidiPlayback: {
+    text: 'MIDI-Wiedergabe',
+    description: 'Einstellungen für MIDI-Wiedergabe',
+  },
+  showMidiPlaybackContextualBubble: {
+    text: 'Wiedergabeblase anzeigen',
+    description:
+      'Ein kleines Wiedergabe-Icon (kleine Blase in der Ecke links unten) ' +
+      'wird angezeigt; durck Mausklick (oder Leertaste) wird die ' +
+      'MIDI-Wiedergabeleiste angezeigt und MIDI-die Wiedergabe gestartet.',
+  },
+  showMidiPlaybackControlBar: {
+    text: 'MIDI-Wiedergabeleiste anzeigen',
+    description: 'MIDI-Wiedergabeleiste anzeigen',
+  },
+  scrollFollowMidiPlayback: {
+    text: 'MIDI-Wiedergabe mitscrollen',
+    description: 'Die angezeigte Notation folgt der MIDI-Wiedergabe (scrollen)',
+  },
+  pageFollowMidiPlayback: {
+    text: 'MIDI-Wiedergabe mitblättern',
+    description: 'Die angezeigte Notation blättert, um der MIDI-Wiedergabe zu folgen',
+  },
+  highlightCurrentlySoundingNotes: {
+    text: 'Klingende Noten hervorheben',
+    description: 'Aktuell klingende Noten im Notationsfeld visuell hervorheben',
+  },
+  // Transposition
+  titleTransposition: {
+    text: 'Transposition',
+    description: 'Notation transponieren',
+  },
+  enableTransposition: {
+    text: 'Transposition aktivieren',
+    description:
+      'Transposition aktivieren, die über die Schaltfläche ' + 
+      '"Transponieren" weiter unten angewendet werden soll. ' +
+      'Die Transposition wird nur auf die Notation angewendet, ' + 
+      'die Kodierung bleibt unverändert, es sei denn, Sie klicken ' + 
+      'im Dropdown-Menü "Manipulieren" auf den Eintrag "Mit Verovio neu rendern".',
+  },
+  transposeInterval: {
+    text: 'Nach Intervallen',
+    description:
+      'Transponieren der Kodierung nach chromatischen Intervallen ' + 
+      'durch die gebräuchlichsten Intervalle (Verovio unterstützt das Basis-40-System)',
+    labels: [
+      'Reine Prim',
+      'Übermässige Prim',
+      'Verminderte Sekund',
+      'Kleine Sekund',
+      'Große Sekund',
+      'Übermässige Sekund',
+      'Verminderte Terz',
+      'Kleine Terz',
+      'Große Terz',
+      'Übermässige Terz',
+      'Verminderte Quart',
+      'Reine Quart',
+      'Übermässige Quart',
+      'Verminderte Quint',
+      'Reine Quint',
+      'Übermässige Quint',
+      'Verminderte Sext',
+      'Kleine Sext',
+      'Große Sext',
+      'Übermässige Sext',
+      'Verminderte Sept',
+      'Kleine Sept',
+      'Große Sept',
+      'Übermässige Sept',
+      'Verminderte Oktav',
+      'Reine Oktav',
+    ],
+  },
+  transposeKey: {
+    text: 'Transponieren zur Tonart',
+    description: 'Transponieren zur Tonart',
+    labels: [
+      'Cis-Dur / ais-Moll',
+      'Fis-Dur / dis-Moll',
+      'H-Dur / gis-Moll',
+      'E-Dur / cis-Moll',
+      'A-Dur / fis-Moll',
+      'D-Dur / h-Moll',
+      'G-Dur / e-Moll',
+      'C-Dur / a-Moll',
+      'F-Dur / d-Moll',
+      'B-Dur / g-Moll',
+      'Es-Dur / c-Moll',
+      'As-Dur / f-Moll',
+      'Des-Dur / b-Moll',
+      'Ges-Dur / es-Moll',
+      'Ces-Dur / as-Moll',
+    ],
+  },
+  transposeDirection: {
+    text: 'Transpositionsrichtung',
+    description: 'Transpositionsrichtung (Hinauf/hinunter)',
+    labels: ['Hinauf', 'Hinunter', 'Näheste'],
+  },
+  transposeButton: {
+    text: 'Transponieren',
+    description:
+      'Transposition mit obigen Einstellungen auf die Notation anwenden, ' + 
+      'die MEI-Kodierung bleibt unverändert. Um auch die MEI-Kodierung ' + 
+      'mit den aktuellen Einstellungen zu transponieren, verwenden Sie ' + 
+      '"Mit Verovio neu rendern" im Dropdown-Menü "Manipulieren".',
+  },
+  // Renumber measures
+  renumberMeasuresHeading: {
+    text: 'Renumber measures',
+    description: 'Settings for renumbering measures',
+  },
+  renumberMeasureContinueAcrossIncompleteMeasures: {
+    text: 'Continue across incomplete measures',
+    description: 'Continue measure numbers across incomplete measures (@metcon="false")',
+  },
+  renumberMeasuresUseSuffixAtMeasures: {
+    text: 'Suffix at incomplete measures',
+    description: 'Use number suffix at incomplete measures (e.g., 23-cont)',
+  },
+  renumberMeasuresContinueAcrossEndings: {
+    text: 'Continue across endings',
+    description: 'Continue measure numbers across endings',
+  },
+  renumberMeasuresUseSuffixAtEndings: {
+    text: 'Suffix at endings',
+    description: 'Use number suffix at endings (e.g., 23-a)',
+  },
+  // Annotations
+  titleAnnotations: {
+    text: 'Annotations',
+    description: 'Annotation settings',
+  },
+  showAnnotations: {
+    text: 'Show annotations',
+    description: 'Show annotations in notation',
+  },
+  showAnnotationPanel: {
+    text: 'Show annotation panel',
+    description: 'Show annotation panel',
+  },
+  annotationDisplayLimit: {
+    text: 'Maximum number of annotations',
+    description: 'Maximum number of annotations to display ' + '(large numbers may slow mei-friend)',
+  },
+  // Facsimile
+  titleFacsimilePanel: {
+    text: 'Facsimile panel',
+    description: 'Show the facsimile imiages of the source edition, if available',
+  },
+  showFacsimilePanel: {
+    text: 'Show facsimile panel',
+    description: 'Show the score images of the source edition provided in the facsimile element',
+  },
+  selectFacsimilePanelOrientation: {
+    text: 'Facsimile panel position',
+    description: 'Select facsimile panel position relative to notation',
+    labels: ['left', 'right', 'top', 'bottom'],
+  },
+  facsimileZoomInput: {
+    text: 'Facsimile image zoom (%)',
+    description: 'Zoom level of facsimile image (in percent)',
+  },
+  showFacsimileFullPage: {
+    text: 'Show full page',
+    description: 'Show facsimile image on full page',
+  },
+  showFacsimileZones: {
+    text: 'Show facsimile zone boxes',
+    description: 'Show facsimile zone bounding boxes',
+  },
+  editFacsimileZones: {
+    text: 'Edit facsimile zones',
+    description: 'Edit facsimile zones (will link bounding boxes to facsimile zones)',
+  },
+  // Supplied element
+  titleSupplied: {
+    text: 'Handle editorial content',
+    description: 'Control handling of <supplied> elements',
+  },
+  showSupplied: {
+    text: 'Show <supplied> elements',
+    description: 'Highlight all elements contained by a <supplied> element',
+  },
+  suppliedColor: {
+    text: 'Select <supplied> highlight color',
+    description: 'Select <supplied> highlight color',
+  },
+  respSelect: {
+    text: 'Select <supplied> responsibility',
+    description: 'Select responsibility id',
+  },
 };
--- app/static/lang/lang.en.js
+++ app/static/lang/lang.en.js
@@ -1,6 +1,8 @@
 /**
  * Language file for English
  */
+import * as att from '../lib/attribute-classes.js';
+
 export const lang = {
   // Main menu bar
   GithubLoginLink: { text: 'Login' },
@@ -48,7 +50,10 @@ export const lang = {
   FacsimileRight: { text: 'Facsimile right' },
 
   // mei-friend SETTINGS MENU
-  //   titleGeneral: { text: 'General', description: 'General mei-friend settings' },
+  titleGeneral: {
+    text: 'General',
+    description: 'General mei-friend settings',
+  },
   selectToolkitVersion: {
     text: 'Verovio version',
     description:
@@ -80,5 +85,245 @@ export const lang = {
       'application name, version, date of first ' +
       'and last edit',
   },
-  selectLanguage: { text: 'Language', description: 'Select language of mei-friend interface.' },
+  selectLanguage: {
+    text: 'Language',
+    description: 'Select language of mei-friend interface.',
+  },
+  // Drag select
+  dragSelection: {
+    text: 'Drag select',
+    description: 'Select elements in notation with mouse drag',
+  },
+  dragSelectNotes: {
+    text: 'Select notes',
+    description: 'Select notes',
+  },
+  dragSelectRests: {
+    text: 'Select rests',
+    description: 'Select rests and repeats (rest, mRest, beatRpt, halfmRpt, mRpt)',
+  },
+  dragSelectControlElements: {
+    text: 'Select placement elements ',
+    description: 'Select placement elements (i.e., with a @placement attribute: ' + att.attPlacement.join(', ') + ')',
+  },
+  dragSelectSlurs: {
+    text: 'Select slurs ',
+    description: 'Select slurs (i.e., elements with @curvature attribute: ' + att.attCurvature.join(', ') + ')',
+  },
+  dragSelectMeasures: {
+    text: 'Select measures ',
+    description: 'Select measures',
+  },
+  // Control menu
+  controlMenuSettings: {
+    text: 'Notation control bar',
+    description: 'Define items to be shown in control menu above the notation',
+  },
+  controlMenuFlipToPageControls: {
+    text: 'Show flip to page controls',
+    description: 'Show flip to page controls in notation control menu',
+  },
+  controlMenuUpdateNotation: {
+    text: 'Show notation update controls',
+    description: 'Show notation update behavior controls in notation control menu',
+  },
+  controlMenuFontSelector: {
+    text: 'Show notation font selector',
+    description: 'Show notation font (SMuFL) selector in notation control menu',
+  },
+  controlMenuNavigateArrows: {
+    text: 'Show navigation arrows',
+    description: 'Show notation navigation arrows in notation control menu',
+  },
+  controlMenuSpeedmodeCheckbox: {
+    text: 'Show speed mode checkbox',
+    description: 'Show speed mode checkbox in notation control menu',
+  },
+  // MIDI Playback
+  titleMidiPlayback: {
+    text: 'MIDI playback',
+    description: 'MIDI playback settings',
+  },
+  showMidiPlaybackContextualBubble: {
+    text: 'Show playback shortcut',
+    description:
+      'Causes a shortcut (bubble in bottom left corner; ' +
+      'click to immediately start playback) to appear ' +
+      'when the MIDI playback control bar is closed',
+  },
+  showMidiPlaybackControlBar: {
+    text: 'Show MIDI playback control bar',
+    description: 'Show MIDI playback control bar',
+  },
+  scrollFollowMidiPlayback: {
+    text: 'Scroll-follow MIDI playback',
+    description: 'Scroll notation panel to follow MIDI playback on current page',
+  },
+  pageFollowMidiPlayback: {
+    text: 'Page-follow MIDI playback',
+    description: 'Automatically flip pages to follow MIDI playback',
+  },
+  highlightCurrentlySoundingNotes: {
+    text: 'Highlight currently-sounding notes',
+    description: 'Visually highlight currently-sounding notes in the notation panel during MIDI playback ',
+  },
+  // Transposition
+  titleTransposition: {
+    text: 'Transpose',
+    description: 'Transpose score information',
+  },
+  enableTransposition: {
+    text: 'Enable transposition',
+    description:
+      'Enable transposition settings, to be applied through the transpose button below. The transposition will be applied to the notation only, the encoding remains unchanged, unless you click the item "Rerender via Verovio" in the "Manipulate" dropdown menu.',
+  },
+  transposeInterval: {
+    text: 'Transpose by interval',
+    description:
+      'Transpose encoding by chromatic interval by the most common intervals (Verovio supports the base-40 system)',
+    labels: [
+      'Perfect Unison',
+      'Augmented Unison',
+      'Diminished Second',
+      'Minor Second',
+      'Major Second',
+      'Augmented Second',
+      'Diminished Third',
+      'Minor Third',
+      'Major Third',
+      'Augmented Third',
+      'Diminished Fourth',
+      'Perfect Fourth',
+      'Augmented Fourth',
+      'Diminished Fifth',
+      'Perfect Fifth',
+      'Augmented Fifth',
+      'Diminished Sixth',
+      'Minor Sixth',
+      'Major Sixth',
+      'Augmented Sixth',
+      'Diminished Seventh',
+      'Minor Seventh',
+      'Major Seventh',
+      'Augmented Seventh',
+      'Diminished Octave',
+      'Perfect Octave',
+    ],
+  },
+  transposeKey: {
+    text: 'Transpose to key',
+    description: 'Transpose to key',
+    labels: [
+      'C# major / A# minor',
+      'F# major / D# minor',
+      'B major / G# minor',
+      'E major / C# minor',
+      'A major / F# minor',
+      'D major / B minor',
+      'G major / E minor',
+      'C major / A minor',
+      'F major / D minor',
+      'Bb major / G minor',
+      'Eb major / C minor',
+      'Ab major / F minor',
+      'Db major / Bb minor',
+      'Gb major / Eb minor',
+      'Cb major / Ab minor',
+    ],
+  },
+  transposeDirection: {
+    text: 'Pitch direction',
+    description: 'Pitch direction of transposition (up/down)',
+    labels: ['Up', 'Down', 'Closest'],
+  },
+  transposeButton: {
+    text: 'Transpose',
+    description:
+      'Apply transposition with above settings to the notation, while the MEI encoding remains unchanged. To also transpose the MEI encoding with the current settings, use "Rerender via Verovio" in the "Manipulate" dropdown menu.',
+  },
+  // Renumber measures
+  renumberMeasuresHeading: {
+    text: 'Renumber measures',
+    description: 'Settings for renumbering measures',
+  },
+  renumberMeasureContinueAcrossIncompleteMeasures: {
+    text: 'Continue across incomplete measures',
+    description: 'Continue measure numbers across incomplete measures (@metcon="false")',
+  },
+  renumberMeasuresUseSuffixAtMeasures: {
+    text: 'Suffix at incomplete measures',
+    description: 'Use number suffix at incomplete measures (e.g., 23-cont)',
+  },
+  renumberMeasuresContinueAcrossEndings: {
+    text: 'Continue across endings',
+    description: 'Continue measure numbers across endings',
+  },
+  renumberMeasuresUseSuffixAtEndings: {
+    text: 'Suffix at endings',
+    description: 'Use number suffix at endings (e.g., 23-a)',
+  },
+  // Annotations
+  titleAnnotations: {
+    text: 'Annotations',
+    description: 'Annotation settings',
+  },
+  showAnnotations: {
+    text: 'Show annotations',
+    description: 'Show annotations in notation',
+  },
+  showAnnotationPanel: {
+    text: 'Show annotation panel',
+    description: 'Show annotation panel',
+  },
+  annotationDisplayLimit: {
+    text: 'Maximum number of annotations',
+    description: 'Maximum number of annotations to display ' + '(large numbers may slow mei-friend)',
+  },
+  // Facsimile
+  titleFacsimilePanel: {
+    text: 'Facsimile panel',
+    description: 'Show the facsimile imiages of the source edition, if available',
+  },
+  showFacsimilePanel: {
+    text: 'Show facsimile panel',
+    description: 'Show the score images of the source edition provided in the facsimile element',
+  },
+  selectFacsimilePanelOrientation: {
+    text: 'Facsimile panel position',
+    description: 'Select facsimile panel position relative to notation',
+    labels: ['left', 'right', 'top', 'bottom'],
+  },
+  facsimileZoomInput: {
+    text: 'Facsimile image zoom (%)',
+    description: 'Zoom level of facsimile image (in percent)',
+  },
+  showFacsimileFullPage: {
+    text: 'Show full page',
+    description: 'Show facsimile image on full page',
+  },
+  showFacsimileZones: {
+    text: 'Show facsimile zone boxes',
+    description: 'Show facsimile zone bounding boxes',
+  },
+  editFacsimileZones: {
+    text: 'Edit facsimile zones',
+    description: 'Edit facsimile zones (will link bounding boxes to facsimile zones)',
+  },
+  // Supplied element
+  titleSupplied: {
+    text: 'Handle editorial content',
+    description: 'Control handling of <supplied> elements',
+  },
+  showSupplied: {
+    text: 'Show <supplied> elements',
+    description: 'Highlight all elements contained by a <supplied> element',
+  },
+  suppliedColor: {
+    text: 'Select <supplied> highlight color',
+    description: 'Select <supplied> highlight color',
+  },
+  respSelect: {
+    text: 'Select <supplied> responsibility',
+    description: 'Select responsibility id',
+  },
 };
