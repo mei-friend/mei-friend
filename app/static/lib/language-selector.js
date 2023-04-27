e54a2ffa2d6fbd91bc9f27072710b825c46180f5
David M. Weigl <weigl@mdw.ac.at>
2023-04-27 23:00:12 +0200

    Selection indicator, link language settings (WIP)

--- app/static/lib/language-selector.js
+++ app/static/lib/language-selector.js
@@ -1,4 +1,5 @@
 import { supportedLanguages } from './defaults.js';
+import { translator } from './main.js'
 
 export function buildLanguageSelection() {
  const langTable = document.querySelector("#languageSelectionList table");
@@ -7,15 +8,41 @@ export function buildLanguageSelection() {
     row.innerHTML = `<td id='lang_${l}_code'>${l}</td>`;
     row.innerHTML += `<td id='lang_${l}_native'>${supportedLanguages[l][l]}</td>`;
     row.innerHTML += `<td id='lang_${l}_selected'/>`;
+    row.id = "row_"+l;
+    row.addEventListener("click", selectLanguage);
     langTable.insertAdjacentElement("beforeend", row);
  })
+ // make default language selected
+ document.getElementById("row"+translator.defaultLangCode)
 } // buildLanguageSelector()
 
 export function translateLanguageSelection(selected = 'en') { 
- const langList = document.getElementById("languageSelectionList");
  Object.keys(supportedLanguages).forEach(l => { 
     const selectedSpan = document.getElementById("lang_" + l + "_selected");
     if(selectedSpan)
         selectedSpan.innerText = supportedLanguages[selected][l];
- })
+ });
+ // clear prev selected row indicator from language table
+ document.querySelectorAll(".selectedLang").forEach(row => row.classList.remove("selectedLang"));
+ // add new selected row indicator
+ const selectedLangRow = document.getElementById("row_"+selected);
+ if(selectedLangRow) { 
+   selectedLangRow.classList.add("selectedLang");
+ }
 } // translateLanguageSelection()
+
+function selectLanguage(e) { 
+   // control language selection in settings based on clicked table row
+   const langSettings = document.getElementById("selectLanguage");
+   const target = e.target.nodeName === "TR" ? e.target : e.target.closest("tr");
+   if(target) { 
+      // set value to native language text (2nd cell in row)
+      langSettings.value = target.querySelector(":nth-child(2)").innerText;
+      // fire input event on language select in settings, to trigger translator
+      const inputEvent = new Event('input');
+      // FIXME the following dispatch does not appear to be working
+      langSettings.dispatchEvent(inputEvent);
+   } else { 
+      console.warn("Couldn't locate target language row on click: ", e);
+   }
+} // selectLanguage()
