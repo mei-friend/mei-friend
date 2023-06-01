2ba386de5a3a69ddd94597a4c6d5548feaef8c06
Werner Goebl <goebl@mdw.ac.at>
2023-06-01 09:30:52 +0200

    Update date

--- app/static/lib/main.js
+++ app/static/lib/main.js
@@ -1,6 +1,6 @@
 // mei-friend version and date
 export const version = '0.8.11';
-export const versionDate = '31 May 2023'; // use full or 3-character english months, will be translated
+export const versionDate = '1 June 2023'; // use full or 3-character english months, will be translated
 
 var vrvWorker;
 var spdWorker;
