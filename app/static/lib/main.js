7f25d919c5b734f89417c7ce5f25907502b7e1c0
David M. Weigl <weigl@mdw.ac.at>
2023-07-31 15:40:07 +0200

    patch gunicorn logging config

--- app/static/lib/main.js
+++ app/static/lib/main.js
@@ -1,6 +1,6 @@
 // mei-friend version and date
-export const version = '0.10.0';
-export const versionDate = '10 July 2023'; // use full or 3-character english months, will be translated
+export const version = '0.10.0-p';
+export const versionDate = '31 July 2023'; // use full or 3-character english months, will be translated
 
 var vrvWorker;
 var spdWorker;
