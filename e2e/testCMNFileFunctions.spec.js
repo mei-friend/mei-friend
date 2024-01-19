ba955bbc9e8a8dc21e699682df893684e1b153f0
David M. Weigl <weigl@mdw.ac.at>
2024-01-19 11:26:34 +0100

    Begin work on testing CMN file functions

---
+++ e2e/testCMNFileFunctions.spec.js
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Test CMN file functions', () => {
+  test('Test OpenURL function to load known CMN MEI', async ({ page }) => {
+    
+  });
+})
