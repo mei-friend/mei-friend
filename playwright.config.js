543e9c00ed6665f494b4c10a4828de6363746f4b
David M. Weigl <weigl@mdw.ac.at>
2024-05-03 14:06:01 +0200

    Read testing baseURL from .env

--- playwright.config.js
+++ playwright.config.js
@@ -5,7 +5,7 @@ const { defineConfig, devices } = require('@playwright/test');
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// require('dotenv').config();
+require('dotenv').config();
 
 /**
  * @see https://playwright.dev/docs/test-configuration
@@ -28,7 +28,7 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:5001',
+    baseURL: process.env.TEST_URL || 'https://mei-friend.mdw.ac.at',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
