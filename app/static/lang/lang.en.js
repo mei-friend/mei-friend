e7458daba4bc0a7960efc0b8bbfd85e16e08a6c4
David M. Weigl <weigl@mdw.ac.at>
2024-12-10 14:42:52 +0100

    Add proposed new splash paragraph regarding GitHub proxy

--- app/static/lang/lang.en.js
+++ app/static/lang/lang.en.js
@@ -29,15 +29,18 @@ export const lang = {
       <p>
         Though mei-friend is a browser-based application, your personal data (including the encoding you
         are editing, your application settings, and current login details if any) are stored in your browser's
-        <a href="https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage" target="_blank"
-          >local storage</a
-        > and are not transmitted to or stored on our servers.
+        <a href="https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage" target="_blank">local storage</a> 
+        and are not stored on our servers.
       </p>
       <p>
         Data is transmitted to GitHub only when you explicitly request it (e.g., when you log in to GitHub, load
         your encoding from or commit to a GitHub repository, or when you request a GitHub Action workflow to be
         run for you). Similarly, data is transmitted to your chosen Solid provider only when you explicitly
-        request it (e.g., when you log in to Solid, or load or save stand-off annotations).
+        request it (e.g., when you log in to Solid, or load or save stand-off annotations).  For technical reasons, 
+        certain interactions with GitHub (cloning a repository to your browser when first opening an encoding, 
+        or committing changes to a repository) require data to be transmitted to a proxy server hosted by the 
+        mdw &ndash; University of Music and Performing Arts Vienna. This server acts as an intermediary between your 
+        browser and GitHub, and does not store any data transmitted through it.
       </p>
       <p>
         We use <a href="https://matomo.org/" target="_blank">Matomo</a>
