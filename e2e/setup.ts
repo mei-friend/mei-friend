cef176230262b7afe157f10fee821a88662e7cb0
David M. Weigl <weigl@mdw.ac.at>
2025-01-20 15:02:50 +0100

    When setting up a page with facsimile, wait for source-image to load before proceeding

--- e2e/setup.ts
+++ e2e/setup.ts
@@ -1,11 +1,15 @@
-import { Page } from "@playwright/test";
+import { Page } from '@playwright/test';
 
-export const baseUrl = process.env.TEST_URL || "https://mei-friend.mdw.ac.at"
+export const baseUrl = process.env.TEST_URL || 'https://mei-friend.mdw.ac.at';
 export const defaultUrl = baseUrl + '/';
 
 export async function setupPage(page: Page, url = defaultUrl) {
   await page.goto(url);
   await page.click('#splashConfirmButton');
+  // if the #facsimile-panel is visible, wait until the <image> element with id "source-image" is fully loaded
+  if (await page.isVisible('#facsimile-panel')) {
+    await page.waitForSelector('#source-image', { state: 'visible' });
+  }
 }
 
 export async function openUrl(page: Page, url: string) {
@@ -18,4 +22,3 @@ export async function openUrl(page: Page, url: string) {
   await page.keyboard.type(url);
   await page.click('#openUrlButton');
 }
-  
