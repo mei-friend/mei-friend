6b8a2b92fa70a9018b5be96d2b1675f10e1d871c
Werner Goebl <goebl@mdw.ac.at>
2025-02-11 14:37:19 +0100

    Update date and changelog

--- app/static/lib/main.js
+++ app/static/lib/main.js
@@ -1,6 +1,6 @@
 // mei-friend version and date
 export const version = '1.2.2';
-export const versionDate = '10 February 2025'; // use full or 3-character english months, will be translated
+export const versionDate = '11 February 2025'; // use full or 3-character english months, will be translated
 export const splashDate = '17 January 2025'; // date of the splash screen content, same translation rules apply
 
 var vrvWorker;
