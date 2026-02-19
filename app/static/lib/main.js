04ca44214ad5f6e852de09148832144545048c70
David M. Weigl <weigl@mdw.ac.at>
2026-02-19 13:19:17 +0100

    Update date

--- app/static/lib/main.js
+++ app/static/lib/main.js
@@ -1,6 +1,6 @@
 // mei-friend version and date
 export const version = '1.3.0';
-export const versionDate = '11 February 2026'; // use full or 3-character english months, will be translated
+export const versionDate = '19 February 2026'; // use full or 3-character english months, will be translated
 export const splashDate = '17 January 2025'; // date of the splash screen content, same translation rules apply
 
 var vrvWorker;
